52235e52cb6f0e5a924a68acf3d21c8ed426d8a3
Paresh Kumar Rout <routpareshkumar737@gmail.com>
2025-02-03 19:53:29 +0530

    update

--- terraform.tf
+++ terraform.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "deepseek_sg" {
   }
 }
 resource "aws_instance" "deepseek_model" {
-  ami           = "ami-xyz"
+  ami           = "ami-00bb6a80f01f03502"
   instance_type = "g4dn.xlarge"
   key_name      = "my-key"
   security_groups = [aws_security_group.deepseek_sg.name]
@@ -30,7 +30,7 @@ resource "aws_lb" "deepseek_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups   = [aws_security_group.deepseek_sg.id]
-  subnets            = ["subnet-xxxxxx", "subnet-yyyyyy"]
+  subnets            = ["subnet-0e6c7ccb2e0b9e48e", "subnet-01f42a6cc1087ab87"]
   enable_deletion_protection = false
   enable_cross_zone_load_balancing = true
 }
@@ -38,7 +38,7 @@ resource "aws_lb_target_group" "deepseek_target_group" {
   name     = "deepseek-target-group"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = "vpc-xxxxxx"
+  vpc_id   = "vpc-0c1ae7114c8b95d15"
 }
 resource "aws_lb_listener" "deepseek_listener" {
   load_balancer_arn = aws_lb.deepseek_lb.arn
